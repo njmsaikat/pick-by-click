4c461d71ae1847e00f4ff47eecafe804e06f1bce
Saikat Roy <njmsaikat@gmail.com>
2020-04-22 21:08:19 +0600

    2nd slider done
    second slider and features more to come

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ $('.slider-one')
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next",
 });
+
+/* Second Slider */
+
+$('.slider-two')
+.not(".slick-initialized")
+.slick({
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000
+});
+
